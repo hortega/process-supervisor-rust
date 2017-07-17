d879c1e7d7d8aa6deac3f644fb94ff24d340a811
Hector Ortega <hector.oj@gmail.com>
2017-07-17 05:14:47 -0500

    Remove traces

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ use std::thread;
 use rocket::State;
 use rocket_contrib::{Json, Value};
 use std::time::Duration;
-use std::process::Command;
+use std::process::{Command, Child};
 
 #[get("/ping")]
 pub fn ping() -> &'static str {
@@ -52,16 +52,25 @@ fn rocket_with_repository() -> rocket::Rocket {
     rocket(Arc::new(RwLock::new(CommandRepository::new())))
 }
 
+fn start_command(command_name: &String, command_metadata: &CommandMetadata) -> Child {
+    let full_command_name = CommandRepository::
+        build_fully_qualified_command(command_name, command_metadata);
+    let args = CommandRepository::get_arguments(command_metadata);
+    let process = Command::new(full_command_name)
+        .args(args)
+        .spawn()
+        .expect("failed to execute child");
+    println!("New command started, ID: {:?}", process.id());
+    process
+}
+
 fn supervisor(command_repository_clone: &Arc<RwLock<CommandRepository>>) {
-    let mut write_repo_wrapped = command_repository_clone.write();
-    let mut write_repo = write_repo_wrapped.unwrap();
+    let mut write_repo = command_repository_clone.write().unwrap();
     let command_names = write_repo.read_command_names();
-    println!("In loop. Commands: {:?}", command_names);
     for command_name in command_names {
         let command = write_repo.take_from_name(&command_name);
         if command.is_some() {
-            let mut command = command.unwrap();
-            println!("****  Command state {:?}", command.command_metadata.state);
+            let command = command.unwrap();
             let mut new_command = command_repository::Command {
                 command_name: command_name.clone(),
                 command_metadata: command.command_metadata.clone(),
@@ -69,51 +78,30 @@ fn supervisor(command_repository_clone: &Arc<RwLock<CommandRepository>>) {
             };
             match command.process {
                 Some(mut process) => {
-
                     let is_stopped = match process.try_wait() {
                         Ok(Some(_)) => true,
                         Ok(None) => false,
                         Err(_) => true
                     };
                     if is_stopped && command.command_metadata.state == "running" {
-                        println!("**** Child process exists in model, is stopped and should be running");
                         // Process is not running but should be running
-                        let full_command_name = CommandRepository::
-                        build_fully_qualified_command(&command_name, &command.command_metadata);
-                        println!("**** Starting command {:?}", full_command_name);
-                        let args = CommandRepository::get_arguments(&command.command_metadata);
-                        process = Command::new(full_command_name)
-                            .args(args)
-                            .spawn()
-                            .expect("failed to execute child");
-
-                        new_command.process = Some(process);
+                        new_command.process = Some(start_command(&command_name, &command.command_metadata));
                     } else if !is_stopped && command.command_metadata.state == "stopped" {
-                        println!("**** Child process exists in model, is running and should be stopped");
                         // Process is running but shouldn't be
                         process.kill().unwrap();
                         new_command.process = None;
                     } else {
-                        println!("**** Child process exists in model, no action is needed {:?}", process.id());
+                        // The process is in the same state as required by the model, so no
+                        // action is required
                         new_command.process = Some(process);
                     }
                     write_repo.commands.insert(new_command);
                 },
                 None => {
-                    println!("**** Child process DOES NOT exists in model");
                     // Process is not running
                     if command.command_metadata.state == "running" {
-                        println!("**** Child process DOES NOT exists in model and should be running");
                         // But should be running
-                        let full_command_name = CommandRepository::
-                        build_fully_qualified_command(&command_name, &command.command_metadata);
-                        let args = CommandRepository::get_arguments(&command.command_metadata);
-                        let process = Command::new(full_command_name)
-                            .args(args)
-                            .spawn()
-                            .expect("failed to execute child");
-                        println!("**** New child command id {:?}", process.id());
-                        new_command.process = Some(process);
+                        new_command.process = Some(start_command(&command_name, &command.command_metadata));
                     }
                     write_repo.commands.insert(new_command);
                 }
